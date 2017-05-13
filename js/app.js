4efa7389ab184f61220f7be712ec7ffbe2e9828a
Adam Chambers <adammchambers@gmail.com>
2017-05-13 12:01:59 -0400

    Install and configure headroom

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ import 'babel-polyfill'
 // ==========================================================================
 // import EmojiGrid from '../components/Emoji-grid'
 import Parallaxease from 'parallaxease'
+import Headroom from 'headroom.js'
 
 // new EmojiGrid({
 //   step1: '[data-step="1"]',
@@ -16,6 +17,20 @@ import Parallaxease from 'parallaxease'
 // })
 
 $(function() {
+
+  // init headroom
+  const headroom  = new Headroom(
+    document.querySelector('[data-headroom]'),
+    {
+      classes: {
+        pinned: 'Header--pinned',
+        unpinned: 'Header--unpinned'
+      }
+    }
+  )
+  headroom.init()
+
+  // init parallaxease
   new Parallaxease({
     breakpoint: '768px'
   })
--- js/vendor.js
+++ js/vendor.js
@@ -1,3 +1,4 @@
 module.exports = [
-  'parallaxease'
+  'parallaxease',
+  'headroom.js'
 ]
