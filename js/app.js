5e48411633508ac7eeba26940b321e09e336bc72
Adam Chambers <adammchambers@gmail.com>
2017-05-13 11:44:16 -0400

    Install and configure parallax on masthead reccos and panel image

--- js/app.js
+++ js/app.js
@@ -3,19 +3,20 @@ import 'babel-polyfill'
 // ==========================================================================
 // Components
 // ==========================================================================
-import EmojiGrid from '../components/Emoji-grid'
-// import Slack from '../components/Slack'
+// import EmojiGrid from '../components/Emoji-grid'
+import Parallaxease from 'parallaxease'
 
-new EmojiGrid({
-  step1: '[data-step="1"]',
-  step2: '[data-step="2"]',
-  grid: '[data-emoji-grid]',
-  reccos: '[data-reccos]',
-  search: '[data-search]',
-  restart: '[data-restart]'
-})
-
-// new Slack({
-//   msgs: '[data-slack-msg]',
-//   randomMsgs: '[data-msg="random"]'
+// new EmojiGrid({
+//   step1: '[data-step="1"]',
+//   step2: '[data-step="2"]',
+//   grid: '[data-emoji-grid]',
+//   reccos: '[data-reccos]',
+//   search: '[data-search]',
+//   restart: '[data-restart]'
 // })
+
+$(function() {
+  new Parallaxease({
+    breakpoint: '768px'
+  })
+})
