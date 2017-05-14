73537e93c653e7e14b8f3487951239e208726c7d
Adam Chambers <adammchambers@gmail.com>
2017-05-14 10:33:51 -0400

    Build reccos markup and add to masthead list, tweak styles for embed

--- components/Masthead/index.js
+++ components/Masthead/index.js
@@ -5,7 +5,8 @@ export default class Masthead {
   constructor(opts) {
     this.randomEmoji = this.randomEmoji(emojis.availableEmojis())
     this.ui = {
-      emoji: document.querySelector(opts.emoji)
+      emoji: document.querySelector(opts.emoji),
+      reccos: document.querySelector(opts.reccos)
     }
 
     this.ui.emoji.innerHTML = this.randomEmoji[1]
@@ -13,8 +14,27 @@ export default class Masthead {
   }
 
   getRecommendations(emoji) {
+    let markup = ''
     $.get(`/api/recommendations/tracks/${emoji}`, data => {
-      console.log(data)
+      markup = data.items.slice(0, 2).map(item => {
+        return `
+          <li class="Masthead-recommendations__item">
+            <div
+              class="Masthead-recommendations__embed"
+              data-parallax='{"y": -60}'>
+              <iframe src="https://embed.spotify.com/?uri=${item.embed}"
+      						width="315"
+      						height="390
+      						frameborder="0"
+      						style="border: 0;"
+      						allowtransparency="true">
+      				</iframe>
+            </div>
+          </li>
+        `
+      })
+
+      this.ui.reccos.innerHTML = markup.join('')
     })
   }
 
